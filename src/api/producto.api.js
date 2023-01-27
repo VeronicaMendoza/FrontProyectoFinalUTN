20a95b9d99cd4f4b5a395027679a77279af391a3
Vero <mendozavero04@gmail.com>
2023-01-26 21:39:04 -0300

    modificando la ruta

--- src/api/producto.api.js
+++ src/api/producto.api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const getProductosRequest = async () =>
-  await axios.get("http://localhost:3001/productos");
+  await axios.get("https://backproyectofinalutn-production.up.railway.app/productos");
 
 export const createProductoRequest = async (producto) =>
-  await axios.post("http://localhost:3001/productos", producto);
+  await axios.post("https://backproyectofinalutn-production.up.railway.app/productos", producto);
 
 export const deleteProductoRequest = async (id) =>
-  await axios.delete(`http://localhost:3001/productos/${id}`);
+  await axios.delete(`https://backproyectofinalutn-production.up.railway.app/productos/${id}`);
 
 export const getProductoRequest = async (id) =>
-  await axios.get(`http://localhost:3001/productos/${id}`);
+  await axios.get(`https://backproyectofinalutn-production.up.railway.app/productos/${id}`);
 
 export const updateProductoRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:3001/productos/${id}`, newFields);
+  await axios.put(`https://backproyectofinalutn-production.up.railway.app/productos/${id}`, newFields);
 
 export const toggleProductoDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:3001/productos/${id}`, {
+  await axios.put(`https://backproyectofinalutn-production.up.railway.app/productos/${id}`, {
     done,
   });
